e521fd027cd9b4fbc9b87fe97f719e2cce10a7e1
Hakan İnciman <98914185+hincim@users.noreply.github.com>
2023-02-21 18:40:39 +0300

    initial commit

--- app/src/main/java/com/hakaninc/messengerapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/hakaninc/messengerapp/fragments/SettingsFragment.kt
@@ -1,30 +1,32 @@
 package com.hakaninc.messengerapp.fragments
 
+import android.Manifest
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.material.snackbar.Snackbar
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.hakaninc.messengerapp.databinding.FragmentSettingsBinding
 import com.hakaninc.messengerapp.model.Users
-import com.squareup.picasso.Picasso
+import java.io.IOException
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,15 +43,12 @@ class SettingsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private var fragmentSettingsbinding: FragmentSettingsBinding?= null
-    var usersReference : DatabaseReference ?= null
-    var firebaseUser : FirebaseUser ?= null
-    private lateinit var permissionLauncher: ActivityResultLauncher<String>
-    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
-    var selectedPicture: Uri? = null
-    private var storageRef : StorageReference ?= null
-
-
+    private var fragmentSettingsbinding: FragmentSettingsBinding? = null
+    var usersReference: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+    var selected: Uri? = null
+    private var storage: StorageReference? = null
+    private var coverChecker : String ?= ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,28 +64,28 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentSettingsBinding.inflate(LayoutInflater.from(context),container,false)
+        val binding =
+            FragmentSettingsBinding.inflate(LayoutInflater.from(context), container, false)
         fragmentSettingsbinding = binding
 
 
-        registerLauncherProfile()
 
-        storageRef = FirebaseStorage.getInstance().reference.child("User Images")
+        storage = FirebaseStorage.getInstance().reference.child("User Images")
         firebaseUser = FirebaseAuth.getInstance().currentUser
         usersReference = FirebaseDatabase.getInstance().reference.child("Users")
             .child(firebaseUser!!.uid)
 
-        usersReference!!.addValueEventListener(object : ValueEventListener{
+        usersReference!!.addValueEventListener(object : ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
 
-                if (snapshot.exists()){
-                    val user : Users? = snapshot.getValue(Users::class.java)
+                if (snapshot.exists()) {
+                    val user: Users? = snapshot.getValue(Users::class.java)
 
-                    if (context != null){
+                    if (context != null) {
                         binding.usernameSettings.text = user!!.username
-                    //  Picasso.get().load(user.profile).into(binding.profileImageSettings)
-                    //  Picasso.get().load(user.cover).into(binding.coverImageSettings)
+                        //  Picasso.get().load(user.profile).into(binding.profileImageSettings)
+                        //  Picasso.get().load(user.cover).into(binding.coverImageSettings)
                     }
                 }
             }
@@ -101,7 +100,8 @@ class SettingsFragment : Fragment() {
             pickImageProfile()
         }
 
-        binding.coverImageSettings .setOnClickListener {
+        binding.coverImageSettings.setOnClickListener {
+            coverChecker = "cover"
             pickImageCover()
         }
 
@@ -110,48 +110,44 @@ class SettingsFragment : Fragment() {
 
     private fun pickImageCover() {
 
-            if (context?.let { ContextCompat.checkSelfPermission(it, android.Manifest.permission.READ_EXTERNAL_STORAGE) } != PackageManager.PERMISSION_GRANTED) {
-
-                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-                    Snackbar.make(fragmentSettingsbinding!!.coverImageSettings, "İzin tekrar istensin mi?", Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Evet", View.OnClickListener {
-                            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                        })
-                        .show()
-
-                } else {
-                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                }
-
-            } else {
-
-                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                activityResultLauncher.launch(intentToGallery)
-            }
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        } else {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 2)
+            // bize sonuç verecek bir activity başlat.
         }
+    }
 
 
     private fun pickImageProfile() {
 
-        if (context?.let { ContextCompat.checkSelfPermission(it, android.Manifest.permission.READ_EXTERNAL_STORAGE) } != PackageManager.PERMISSION_GRANTED) {
-
-            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
-
-                Snackbar.make(fragmentSettingsbinding!!.profileImageSettings, "İzin tekrar istensin mi?", Snackbar.LENGTH_INDEFINITE)
-                    .setAction("Evet", View.OnClickListener {
-                        permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    })
-                    .show()
-
-            } else {
-                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-            }
-
+        if (context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            } != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                3
+            )
         } else {
-
-            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            activityResultLauncher.launch(intentToGallery)
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, 4)
+            // bize sonuç verecek bir activity başlat.
         }
     }
 
@@ -175,75 +171,99 @@ class SettingsFragment : Fragment() {
             }
     }
 
-    private fun registerLauncherProfile(){
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { sonuc ->
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-                if (sonuc.resultCode == AppCompatActivity.RESULT_OK) {
-                    // kullanıcı galeriye gidip işlemi yaptıysa
-                    val intentFromResult = sonuc.data
-                    if (intentFromResult != null) {
-                        selectedPicture = intentFromResult.data
-                        selectedPicture?.let {
-                            fragmentSettingsbinding!!.profileImageSettings.setImageURI(selectedPicture)
-                        }
-                    }
-                }else if (sonuc.resultCode == AppCompatActivity.RESULT_OK){
-                    val intentFromResult = sonuc.data
-                    if (intentFromResult != null) {
-                        selectedPicture = intentFromResult.data
-                        selectedPicture?.let {
-                            fragmentSettingsbinding!!.coverImageSettings.setImageURI(selectedPicture)
-                        }
-                    }
-                }
+        if (requestCode == 1){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 2)
+                // bize sonuç verecek bir activity başlat.
             }
-
-
-        permissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { sonuc ->
-
-                if (sonuc) {
-                    val intentToGallery =
-                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-
-                    activityResultLauncher.launch(intentToGallery)
-
-                } else {
-                    Toast.makeText(requireContext(), "İzine ihtiyacım var!", Toast.LENGTH_SHORT).show()
-                }
+        }else if (requestCode == 3){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 4)
+                // bize sonuç verecek bir activity başlat.
             }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
-    private fun registerLauncherCover(){
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { sonuc ->
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
-                if (sonuc.resultCode == AppCompatActivity.RESULT_OK) {
-                    val intentFromResult = sonuc.data
-                    if (intentFromResult != null) {
-                        selectedPicture = intentFromResult.data
-                        selectedPicture?.let {
-                            fragmentSettingsbinding!!.coverImageSettings.setImageURI(selectedPicture)
-                        }
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            selected = data.data
+            Toast.makeText(context,"Uploading",Toast.LENGTH_SHORT).show()
+            uploadImageToDatabase()
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, selected)
+                fragmentSettingsbinding!!.coverImageSettings.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }else {
+            selected = data?.data
+            Toast.makeText(context,"Uploading",Toast.LENGTH_SHORT).show()
+            uploadImageToDatabase()
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, selected)
+                fragmentSettingsbinding!!.profileImageSettings.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun uploadImageToDatabase() {
+        val progressBar = ProgressDialog(context)
+        progressBar.setMessage("image is uploading, please wait...")
+        progressBar.show()
+        println(selected)
+        println("errorrrrr")
+
+        if (selected != null){
+            val fileRef = storage?.child(System.currentTimeMillis().toString() + ".jpg")
+            var uploadTask : StorageTask<*>
+            uploadTask = fileRef!!.putFile(selected!!)
+
+            uploadTask.continueWithTask((Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task ->
+                if (!task.isSuccessful){
+
+                    task.exception?.let {
+                        throw it
                     }
                 }
-            }
+                return@Continuation fileRef.downloadUrl
 
+            })).addOnCompleteListener { task ->
+                if (task.isSuccessful){
 
-        permissionLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { sonuc ->
+                    val downloadUrl = task.result
+                    val url = downloadUrl.toString()
 
-                if (sonuc) {
-                    val intentToGallery =
-                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    if (coverChecker == "cover"){
 
-                    activityResultLauncher.launch(intentToGallery)
+                        val mapCoverImg = HashMap<String,Any>()
+                        mapCoverImg["cover"] = url
+                        usersReference!!.setValue(mapCoverImg)
+                        coverChecker = ""
 
-                } else {
-                    Toast.makeText(requireContext(), "İzine ihtiyacım var!", Toast.LENGTH_SHORT).show()
+                    }else{
+
+                        val mapProfileImg = HashMap<String,Any>()
+                        mapProfileImg["profile"] = url
+                        usersReference!!.setValue(mapProfileImg)
+                        coverChecker = ""
+
+                    }
+                    progressBar.dismiss()
                 }
             }
+        }
     }
 
 
